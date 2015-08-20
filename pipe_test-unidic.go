da03eea8e415aeda49d5ef0468be733722fb2cee
hydrocul <hydrocul@gmail.com>
2015-08-21 00:12:17 +0900

    テストに失敗していたのを訂正

--- pipe_test-unidic.go
+++ pipe_test-unidic.go
@@ -5,9 +5,9 @@ import "testing"
 func TestPipe(t *testing.T) {
 	dict := loadDictionary()
 	_testPipeParse(t, dict, "形態素解析", "|形態|素|解析|", 1000)
-	_testPipeParse(t, dict, "貴社の記者が汽車で帰社した", "|貴社|の|記者|が|汽車|で|帰社|した|", 1000)
-	_testPipeParse(t, dict, "貴社の記者が汽車で帰社した", "|貴社|の|記者|が|汽車|で|帰社|した|", 9)
-	_testPipeParse(t, dict, "貴社の記者が汽車で帰社した", "|貴社|の|記者|が|汽車|で|帰社|した|", 10)
+	_testPipeParse(t, dict, "貴社の記者が汽車で帰社した", "|貴社|の|記者|が|汽車|で|帰社|し|た|", 1000)
+	_testPipeParse(t, dict, "貴社の記者が汽車で帰社した", "|貴社|の|記者|が|汽車|で|帰社|し|た|", 9)
+	_testPipeParse(t, dict, "貴社の記者が汽車で帰社した", "|貴社|の|記者|が|汽車|で|帰社|し|た|", 10)
 	_testPipeParse(t, dict, "漢字未知語のテスト避筌析テスト", "|漢字|未知|語|の|テスト|?避|?筌|?析|テスト|", 1000)
 }
 
